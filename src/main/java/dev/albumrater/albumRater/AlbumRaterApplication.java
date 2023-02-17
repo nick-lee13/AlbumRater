ccdcf805cd49f080f6b6af708fddde981af173b2
nick-lee13 <cadet.lee1@gmail.com>
2023-02-17 17:22:08 -0400

    Spring init

---
+++ src/main/java/dev/albumrater/albumRater/AlbumRaterApplication.java
@@ -0,0 +1,13 @@
+package dev.albumrater.albumRater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumRaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumRaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/albumrater/albumRater/AlbumRaterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.albumrater.albumRater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumRaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
