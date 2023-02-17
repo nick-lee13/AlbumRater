69b33bae056d643aeeacb0c4a6bcc3f6c2c97ecc
nick-lee13 <ntlee@mta.ca>
2023-02-17 16:52:57 -0400

    Import code

---
+++ albumrater/src/test/java/com/nickspicks/albumrater/AlbumraterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickspicks.albumrater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
