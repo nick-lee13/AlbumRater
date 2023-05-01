2b182e33de9a95aec572cc752d31b806857fe8da
nick-lee13 <ntlee@mta.ca>
2023-05-01 20:34:47 -0300

    fixed ngrok issue

--- src/main/java/dev/albumrater/albumRater/AlbumController.java
+++ src/main/java/dev/albumrater/albumRater/AlbumController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/albums")
+@CrossOrigin
 public class AlbumController {
 
     @Autowired
--- src/main/java/dev/albumrater/albumRater/ReviewController.java
+++ src/main/java/dev/albumrater/albumRater/ReviewController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
