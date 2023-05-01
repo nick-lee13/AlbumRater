5925a2b56ad2e5918862695b93e094fe70a73fee
nick-lee13 <ntlee@mta.ca>
2023-05-01 17:12:54 -0300

    Front end commit

--- frontend/album-rater-v1/src/App.js
+++ frontend/album-rater-v1/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [albums, setAlbums] = useState();
+
+  const getAlbums = async () =>{
+
+    try
+    {
+
+      const response = await api.get("/api/v1/albums");
+
+      console.log(response.data);
+
+      setAlbums(response.data);
+
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+
+  }
+
+  useEffect(() => {
+    getAlbums();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
