3e2c39110b662fe79cd77e237e3133ff919cfecc
nick-lee13 <ntlee@mta.ca>
2023-05-03 11:50:12 -0300

    Local host

--- frontend/album-rater-v1/src/api/axiosConfig.js
+++ frontend/album-rater-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://5be7-99-192-40-35.ngrok-free.app',
+    baseURL:'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
